5b9771c2d0de61bc3e27ff5838ffce55be506412
Makuss88 <maciborek.grzegorz@gmail.com>
2020-08-25 16:01:39 +0200

    bugx fixed, and cleanig code

--- pong/script.js
+++ pong/script.js
@@ -1,7 +1,9 @@
 const canvas = document.getElementById('pong');
 const ctx = canvas.getContext('2d');
 
-const user = {
+const FRAME = 50;
+
+const playerA = {
   x: 20,
   y: canvas.height / 2 - 100 / 2,
   w: 20,
@@ -10,13 +12,13 @@ const user = {
   score: 0,
 };
 
-const com = {
+const playerB = {
   x: canvas.width - 20 - 20,
   y: canvas.height / 2 - 100 / 2,
   w: 20,
   h: 100, 
-  color: "RED",
-  score: 0,
+  color: 'RED',
+  score: 4,
 };
 
 const ball = {
@@ -25,7 +27,18 @@ const ball = {
   r: 10,
   velX: 2,
   velY: 2,
-  color: 'white',
+  color: 'WHITE',
+};
+
+const drawBoard = (x, y, w, h, color) => {
+  ctx.fillStyle = color;
+  ctx.fillRect(x, y, w, h);
+};
+
+const drawText = (text, x, y, color) => {
+  ctx.fillStyle = color;
+  ctx.font = "120px fantasy";
+  ctx.fillText(text, x, y);
 };
 
 const drawPlayer = (x, y, w, h, color) => {
@@ -41,23 +54,12 @@ const drawCircle = (x, y, r, color) => {
   ctx.fill();
 };
 
-const drawRect = (x, y, w, h, color) => {
-  ctx.fillStyle = color;
-  ctx.fillRect(x, y, w, h);
-};
-
-const drawText = (text, x, y, color) => {
-  ctx.fillStyle = color;
-  ctx.font = "120px fantasy";
-  ctx.fillText(text, x, y);
-};
-
 const drawGame = () => {
-  drawRect(0,0, canvas.width, canvas.height, "GREEN")
-  drawText(user.score, canvas.width / 4, 100, "BLACK");
-  drawText(com  .score, 3 * canvas.width / 4, 100, "BLACK");
-  drawPlayer(user.x, user.y, user.w, user.h, user.color);
-  drawPlayer(com.x, com.y, com.w, com.h, com.color);
+  drawBoard(0,0, canvas.width, canvas.height, "GREEN");
+  drawText(playerA.score, canvas.width / 4, 100, "BLACK");
+  drawText(playerB  .score, 3 * canvas.width / 4, 100, "BLACK");
+  drawPlayer(playerA.x, playerA.y, playerA.w, playerA.h, playerA.color);
+  drawPlayer(playerB.x, playerB.y, playerB.w, playerB.h, playerB.color);
   drawCircle(ball.x, ball.y, ball.r, ball.color);
 };  
 
@@ -75,23 +77,31 @@ const collision = (ball, player) => {
   return ball.rigth > player.left && ball.button > player.top && ball.left < player.rigth && ball.top < player.button; 
 };
 
-const move = (event) => {
+const movePlayerA = (event) => {
   let rect = canvas.getBoundingClientRect();
-  user.y = event.clientY - rect.top - user.h / 2;
+  playerA.y = event.clientY - rect.top - playerA.h / 2;
 };
+
+const movePlayerB = (event) => {
+  if (event.keyCode === 38) {
+    playerB.y -= 15;
+  }
+  if (event.keyCode === 40) {
+    playerB.y += 15;
+  }
+};
+
+canvas.addEventListener('mousemove', movePlayerA);
+document.addEventListener("keydown", movePlayerB);
 
 const resetBall = () => {
   ball.x = canvas.width / 2;
   ball.y = canvas.height / 2;
-  ball.velX = 2
-  ball.velX = - ball.velX;
-}
-
-canvas.addEventListener('mousemove', move);
+  ball.velX = 2 + Math.floor(2 * Math.random());
+  ball.velY = 2 + Math.floor(2 * Math.random());
+};
 
 const updateGame = () => {
-  com.y = ball.y - 50; //add move com paddle
-
   ball.x += ball.velX;
   ball.y += ball.velY;
   
@@ -99,18 +109,28 @@ const updateGame = () => {
     ball.velY = -ball.velY;
   }
 
-  let player = (ball.x < canvas.height) ? user : com;
+  let player = (ball.x < canvas.height) ? playerA : playerB;
 
   if (collision(ball, player)){
     (ball.velX > 0) ? ball.velX += 0.5 :ball.velX -= 0.5;
      ball.velX = -ball.velX;
   }
 
-  if (ball.x - ball.r < 0){
-    com.score++;  
+  if (ball.x + ball.r > canvas.width){
+    playerA.score++; 
+    if (playerA.score === 5){
+      playerA.score = 0;
+      playerB.score = 0;
+      alert('PLAYER A WINS!');
+    } 
     resetBall();
-  } else if (ball.x + ball.r > canvas.width){
-    user.score++;
+  } else if (ball.x - ball.r < 0){
+    playerB.score++;
+    if (playerB.score === 5){
+      playerA.score = 0;
+      playerB.score = 0;
+      alert('PLAYER B WINS!');
+    } 
     resetBall();
   }
 };
@@ -120,5 +140,4 @@ const render = () => {
   drawGame();
 };
 
-const frame = 50
-setInterval(render, 1000 / frame);
+setInterval(render, 1000 / FRAME);
